d5f1d2d3fa89967b1c97c0b438b3f12a400e3119
Mohamed Cherkaoui <chermed@gmail.com>
2017-02-25 23:37:55 +0000

    [REF] use classes to manage dates and date ranges

--- odootools/odootools.py
+++ odootools/odootools.py
@@ -4,57 +4,153 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import calendar
 
-def dt_range(start, stop=False, exclude_weekdays=[], exclude_dates=[]):
-    exclude_dates = not isinstance(exclude_dates, (list, tuple)) and \
-                    [exclude_dates] or exclude_dates
-    exclude_dates = [isinstance(date, basestring) and datetime.strptime(date,
-                    "%Y-%m-%d") or date for date in exclude_dates]
-    if isinstance(start, basestring):
-        start = datetime.strptime(start, "%Y-%m-%d")
-    if stop and isinstance(stop, basestring):
-        stop = datetime.strptime(stop, "%Y-%m-%d")
-    while not stop or start <= stop:
-        if start.weekday() not in exclude_weekdays:
-            if start not in exclude_dates:
-                yield start.strftime("%Y-%m-%d")
-        start = start + relativedelta(days=1)
 
-def last_day(date):
-    if isinstance(date, basestring):
-        date = datetime.strptime(date, "%Y-%m-%d")
-    day = calendar.monthrange(date.year, date.month)[1]
-    date = date + relativedelta(day=day)
-    return date.strftime("%Y-%m-%d")
+class Date:
+    """docstring for Date"""
 
-def first_day(date):
-    if isinstance(date, basestring):
-        date = datetime.strptime(date, "%Y-%m-%d")
-    date = date + relativedelta(day=1)
-    return date.strftime("%Y-%m-%d")
+    def __init__(self, date, fmt='%Y-%m-%d'):
+        if isinstance(date, datetime):
+            date = date.strftime(fmt)
+        self._date = date
+        self._fmt = fmt
 
-def interval_tuple(date):
-    return first_day(date), last_day(date)
+    @property
+    def value(self):
+        return self._date
 
-def next_day(date):
-    if isinstance(date, basestring):
-        date = datetime.strptime(date, "%Y-%m-%d")
-    date = date + relativedelta(days=1)
-    return date.strftime("%Y-%m-%d")
+    def _apply(self, days=None, day=None, months=None, month=None, years=None, year=None):
+        date = datetime.strptime(self._date, self._fmt)
+        if days is not None:
+            date = date + relativedelta(days=days)
+        if day is not None:
+            date = date + relativedelta(day=day)
+        if months is not None:
+            date = date + relativedelta(months=months)
+        if month is not None:
+            date = date + relativedelta(month=month)
+        if years is not None:
+            date = date + relativedelta(years=years)
+        if year is not None:
+            date = date + relativedelta(year=year)
+        self._date = date.strftime(self._fmt)
+        return self
 
-def next_month(date):
-    if isinstance(date, basestring):
-        date = datetime.strptime(date, "%Y-%m-%d")
-    date = date + relativedelta(months=1)
-    return date.strftime("%Y-%m-%d")
+    def next_day(self, days=1):
+        return self._apply(days=days)
 
-def next_month_last_day(date):
-    if isinstance(date, basestring):
-        date = datetime.strptime(date, "%Y-%m-%d")
-    date = date + relativedelta(months=1)
-    return last_day(date.strftime("%Y-%m-%d"))
+    def prev_day(self, days=1):
+        return self._apply(days=-1 * days)
 
-def next_year(date):
-    if isinstance(date, basestring):
-        date = datetime.strptime(date, "%Y-%m-%d")
-    date = date + relativedelta(years=1)
-    return date.strftime("%Y-%m-%d")
+    def first_day_in_month(self):
+        return self._apply(day=1)
+
+    def last_day_in_month(self):
+        date = datetime.strptime(self._date, self._fmt)
+        day = calendar.monthrange(date.year, date.month)[1]
+        return self._apply(day=day)
+
+    def next_month(self, months=1):
+        return self._apply(months=months)
+
+    def prev_month(self, months=1):
+        return self._apply(months=-1 * months)
+
+    def next_year(self, years=1):
+        return self._apply(years=years)
+
+    def prev_year(self, years=1):
+        return self._apply(years=-1 * years)
+
+    def __add__(self, other):
+        return self._apply(days=other)
+
+    def __str__(self):
+        return self.value
+
+
+class DateTime(Date):
+    """docstring for DateTime"""
+
+    def __init__(self, date, fmt='%Y-%m-%d %H:%M:%S'):
+        Date.__init__(self, date, fmt)
+
+    def _apply(self, days=None, day=None, months=None, month=None, years=None,
+               year=None, seconds=None, second=None, minutes=None, minute=None, hours=None, hour=None):
+        Date._apply(self, days=days, months=months, years=years, day=day)
+        date = datetime.strptime(self._date, self._fmt)
+        if seconds is not None:
+            date = date + relativedelta(seconds=seconds)
+        if second is not None:
+            date = date + relativedelta(second=second)
+        if minutes is not None:
+            date = date + relativedelta(minutes=minutes)
+        if minute is not None:
+            date = date + relativedelta(minute=minute)
+        if hours is not None:
+            date = date + relativedelta(hours=hours)
+        if hour is not None:
+            date = date + relativedelta(hour=hour)
+        self._date = date.strftime(self._fmt)
+        return self
+
+    def next_second(self, seconds=1):
+        return self._apply(seconds=seconds)
+
+    def prev_second(self, seconds=1):
+        return self._apply(seconds=-1 * seconds)
+
+    def first_second_in_day(self):
+        return self._apply(second=0, minute=0, hour=0)
+
+    def last_second_in_day(self):
+        return self._apply(second=59, minute=59, hour=23)
+
+    def next_minute(self, minutes=1):
+        return self._apply(minutes=minutes)
+
+    def prev_minute(self, minutes=1):
+        return self._apply(minutes=-1 * minutes)
+
+    def next_hour(self, hours=1):
+        return self._apply(hours=hours)
+
+    def prev_hour(self, hours=1):
+        return self._apply(hours=-1 * hours)
+
+
+class DateRange:
+    """docstring for DateRange"""
+
+    def __init__(self, start, stop=False, exclude_weekdays=[], exclude_dates=[], fmt='%Y-%m-%d'):
+        if isinstance(start, basestring):
+            start = datetime.strptime(start, fmt)
+        self._start = start
+        if isinstance(stop, basestring):
+            stop = datetime.strptime(stop, fmt)
+        self._stop = stop
+        self._exclude_dates = []
+        if not hasattr(exclude_dates, '__iter__'):
+            exclude_dates = [exclude_dates]
+        for exclude_date in exclude_dates:
+            if isinstance(exclude_date, basestring):
+                exclude_date = datetime.strptime(exclude_date, fmt)
+            self._exclude_dates.append(exclude_date)
+        self._exclude_weekdays = []
+        if not hasattr(exclude_weekdays, '__iter__'):
+            exclude_weekdays = [exclude_weekdays]
+        for exclude_weekday in exclude_weekdays:
+            if isinstance(exclude_weekday, basestring):
+                exclude_weekday = int(exclude_weekday)
+            self._exclude_weekdays.append(exclude_weekday)
+        self._fmt = fmt
+
+    def __str__(self):
+        return "({} - {}) exclude dates {} and weekedays {}".format(
+            self._start, self._stop, self._exclude_dates, self._exclude_weekdays)
+
+    def __iter__(self):
+        while not self._stop or self._start <= self._stop:
+            if self._start.weekday() not in self._exclude_weekdays:
+                if self._start not in self._exclude_dates:
+                    yield self._start.strftime("%Y-%m-%d")
+            self._start = self._start + relativedelta(days=1)
--- tests/test_odootools.py
+++ tests/test_odootools.py
@@ -15,17 +15,49 @@ import unittest
 from odootools import odootools
 
 
-
 class TestOdootools(unittest.TestCase):
 
+    def setUp(self):
+        self.start = '2017-02-27'
+        self.stop = '2017-03-02'
+
     def test_dt_range(self):
-        start = '2017-02-27'
-        stop = '2017-03-02'
-        dates = [x for x in odootools.dt_range(start, stop)]
+        dr = odootools.DateRange(self.start, self.stop)
+        dates = [x for x in dr]
         results = [
             '2017-02-27',
             '2017-02-28',
             '2017-03-01',
             '2017-03-02',
         ]
-        self.assertEqual(dates, results, "dt_range return error")
+        self.assertEqual(dates, results, "DateRange return error, waiting {} and getting {}".format(
+            results, dates
+        ))
+
+    def test_date_next_day(self):
+        date = odootools.Date(self.start).next_day().value
+        result = '2017-02-28'
+        self.assertEqual(date, result, "Date next day return error, waiting {} and getting {}".format(
+            result, date
+        ))
+
+    def test_date_prev_day(self):
+        date = odootools.Date(self.start).prev_day().value
+        result = '2017-02-26'
+        self.assertEqual(date, result, "Date previous day return error, waiting {} and getting {}".format(
+            result, date
+        ))
+
+    def test_date_next_month(self):
+        date = odootools.Date(self.start).next_month().value
+        result = '2017-03-27'
+        self.assertEqual(date, result, "Date next month return error, waiting {} and getting {}".format(
+            result, date
+        ))
+
+    def test_date_next_year(self):
+        date = odootools.Date(self.start).next_year().value
+        result = '2018-02-27'
+        self.assertEqual(date, result, "Date next year return error, waiting {} and getting {}".format(
+            result, date
+        ))
